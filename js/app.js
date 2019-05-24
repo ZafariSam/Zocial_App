54f6b18a7a7753ff5ff6ef07a84c7fcba18bba24
ZafariSam <sam@studiozafari.com>
2019-05-24 12:04:52 +0200

    Vue Template Updates

--- js/app.js
+++ js/app.js
@@ -4,7 +4,8 @@ Vue.component('nav-bar', {
       count: 0
     }
   },
-  template:`<div class="nav-bar">
+  template:`
+        <div class="nav-bar">
           <div style="padding: 20px 10%; display: flex;">
             <a href="/"><img src="img/svg/Zafari_Icon_Color.svg" alt="Zaf Logo White" width="40px" height= "40px"></a>
             <div style="margin-left: auto; display: flex;">
